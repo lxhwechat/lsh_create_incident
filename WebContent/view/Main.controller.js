2ebc80e52f0b03865441f6bd547171eae6f32325
Oliver Ou <oliver.ou@sap.com>
2018-08-12 17:08:07 +0800

    use UI5-Tooling; fixed Create Incident failed

--- WebContent/view/Main.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.controller("sm.itsm.createincident.view.Main", {
-
-    onInit : function() {
-        if (sap.ui.Device.support.touch === false) {
-            this.getView().addStyleClass("sapUiSizeCompact");
-        }
-    }
-});
